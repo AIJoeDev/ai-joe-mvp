1d45c790cd065b5089ce9d9b9a84b37c2ed818de
Jeff <admin@jeffwilliams.dev>
2020-12-02 11:25:12 -0700

    fetch firebase config object from /__/firebase/

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,16 +3,20 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDyRhCBMiQZ5s_oJ3xWjuOOuQgsQ4ZXtxo',
-  authDomain: 'exercise-health.firebaseapp.com',
-  databaseURL: 'https://exercise-health.firebaseio.com',
-  projectId: 'exercise-health',
-  storageBucket: 'exercise-health.appspot.com',
-  messagingSenderId: '285597102570',
-  appId: '1:285597102570:web:d7fad1348f747b62b78293',
-  measurementId: 'G-FES4XE9NVH'
-}
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDyRhCBMiQZ5s_oJ3xWjuOOuQgsQ4ZXtxo',
+//   authDomain: 'exercise-health.firebaseapp.com',
+//   databaseURL: 'https://exercise-health.firebaseio.com',
+//   projectId: 'exercise-health',
+//   storageBucket: 'exercise-health.appspot.com',
+//   messagingSenderId: '285597102570',
+//   appId: '1:285597102570:web:d7fad1348f747b62b78293',
+//   measurementId: 'G-FES4XE9NVH'
+// }
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+// firebase.initializeApp(firebaseConfig)
 // firebase.analytics()
+
+fetch('/__/firebase/init.json').then(async response => {
+  firebase.initializeApp(await response.json())
+})
